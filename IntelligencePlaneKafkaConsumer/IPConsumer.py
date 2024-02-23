3bc22bc9232bccdde89e3a741190bcaa4f1c0d7c
sandeepSbudhya <sndps67@gmail.com>
2024-02-23 03:24:54 -0500

    debugged

--- IntelligencePlaneKafkaConsumer/IPConsumer.py
+++ IntelligencePlaneKafkaConsumer/IPConsumer.py
@@ -7,34 +7,34 @@ import datetime, sys, time
 Create a consumer to consume messages as a daemon process which terminates when its
 default timer runs out or a EndOfStream has been received
 '''
-class Consumer(daemon):
+class IPConsumer(daemon):
 
     '''
     initialize the consumer with kafka server (broker), topic and client_id to help filter in the kafka server (broker)
     '''
-    def __init__(self, bootstrap_servers, topic, client_id, consumerfile, pidfile):
+    def __init__(self, bootstrap_servers, topic, pidfile, consumerfile):
         self.consumerfile = consumerfile
+        self.bootstrap_servers = bootstrap_servers
+        self.topic = topic
+        print("kafka variables: \n")
+        print(bootstrap_servers)
+        print(topic)
         super().__init__(pidfile=pidfile)
-        self.kafkaConsumer = kafkaConsumer(
-            topic,
-            bootstrap_servers = bootstrap_servers,
-            client_id = client_id,
+        self.kafka_consumer = KafkaConsumer(
+            self.topic,
+            bootstrap_servers = self.bootstrap_servers,
             value_deserializer = lambda m: json.loads(m.decode('ascii'))
         )
+        print("kafka initialized...")
 
     '''
     This is a method from the daemon boilerplate class overriden to consume messages
     '''
     def run(self):
-        timeOfLatestMessage = None
-        maxWait = datetime.timedelta(seconds=10)
-        with open(self.consumerfile, 'w+') as file:
-            for message in self.kafkaconsumer:
-                timeOfLatestMessage = datetime.datetime.now()
+        with open(self.consumerfile, "w+") as file:
+            for message in self.kafka_consumer:
+                if 'stop listening' in str(message.value):
+                    break 
                 file.write(str(message.value)+'\n')
-                while(true):
-                    if timeOfLatestMessage + maxWait == datetime.datetime.now():
-                        break
-                break
-        file.close()
+            file.close()
         self.stop()
--- flaskserver.py
+++ flaskserver.py
@@ -1,6 +1,7 @@
-import IntelligencePlaneKafkaConsumer.Consumer as Consumer
+import IntelligencePlaneKafkaConsumer.IPConsumer as IPConsumer
 from flask import Flask
 import datetime, os
+import json
 from threading import Thread
 app = Flask(__name__)
 
@@ -12,10 +13,12 @@ def getInfo():
 def scheduleJob():
     curtimestring = str(datetime.datetime.now())
     serverdir = str(os.getcwd())
-    pidfile = os.path.join(serverdir,'pidfiles/pidfile'+curtimestring+'.pid')
-    consumerfile = os.path.join(serverdir, 'consumerfiles/consumerfile'+curtimestring)
-    kc = Consumer.Consumer('localhost:9092','performance-messages','1', pidfile=pidfile, consumerfile=consumerfile)
-    t = Thread(target=kc.start)
+    pidfile = str(os.path.join(serverdir,'pidfiles/pidfile'+curtimestring+'.pid'))
+    consumerfile = str(os.path.join(serverdir, 'consumerfiles/consumerfile'+curtimestring))
+    print("pidfile: ",pidfile)
+    print("consumerfile: ", consumerfile)
+    ipkc = IPConsumer.IPConsumer(['localhost:9092'], 'performance-messages', pidfile, consumerfile)
+    t = Thread(target=ipkc.start)
     t.daemon = True
     t.start()
     return "<!doctype html><title>scheduled</title><h1>Your job is scheduled</h1>"
